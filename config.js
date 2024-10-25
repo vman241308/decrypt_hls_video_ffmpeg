2f1a8a605fee11b7e6b415c66487b362990d9f13
vman241308 <vman241308@gmail.com>
2024-10-25 08:24:49 +0000

    fix: removed config frontfile link

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // config.js
 const config = {
     siteURL: "https://playback.lifesize.com/#/publicvideo/8993eb09-137c-45d7-89e1-83d93591aca2?vcpubtoken=c992616d-d894-4bd2-9f50-0df88866bd32",
-    frontTSFiles: "https://iospriv-vc.lifesize.com/30f85275-6046-4db1-9800-26e4cb15c1a1/recordings/by-date/2024/04/25/8993eb09-137c-45d7-89e1-83d93591aca2-16-25/mainfeed_pip_1152_iosfeed/",
     decryptedDir: "./decrypted_ts_files",
     tsFilesDir: "./ts_files",
     m3u8FileDir: "./m3u8_files",
